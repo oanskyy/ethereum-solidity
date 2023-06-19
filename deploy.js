a439924f7a91afd1d06e9b8b57dbc2fd12db88a7
oanskyy <oana.anghelachi@gmail.com>
2023-06-18 21:15:44 -0500

    feat: install modules (mocha, ganache, web3)

--- deploy.js
+++ deploy.js
@@ -1,1 +1,5 @@
 // deploy code will go here
+
+const assert = require("assert")
+const ganache = require("ganache-cli") // local ETH test network
+const Web3 = require("web3") // bridge between JS and Solidity
