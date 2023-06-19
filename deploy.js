b5eecbed349d4d3b80dc6973df8eed479e650fd0
oanskyy <oana.anghelachi@gmail.com>
2023-06-18 22:39:57 -0500

    feat: add mocha test example

--- deploy.js
+++ deploy.js
@@ -1,5 +1,4 @@
 // deploy code will go here
-
 const assert = require("assert")
 const ganache = require("ganache-cli") // local ETH test network
 
@@ -7,5 +6,4 @@ const ganache = require("ganache-cli") // local ETH test network
 // const Web3 = require("web3") // bridge between JS and Solidity
 const { Web3 } = require("web3")
 
-
-const web3 = new Web3(ganache.provider) //instance of Web3, tells to attempt to connect to a network;  add provider to connect
+const web3 = new Web3(ganache.provider()) //instance of Web3; attempt to connect to a network;  add provider to connect
