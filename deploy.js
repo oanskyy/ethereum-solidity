5f8c111805395b23a0fe91cb51c465dbd5f4bbb3
oanskyy <oana.anghelachi@gmail.com>
2023-06-18 21:33:40 -0500

    feat: add web3 providers

--- deploy.js
+++ deploy.js
@@ -2,4 +2,10 @@
 
 const assert = require("assert")
 const ganache = require("ganache-cli") // local ETH test network
-const Web3 = require("web3") // bridge between JS and Solidity
+
+// Note - This change will be required for all instances shown throughout the course where web3 is imported.
+// const Web3 = require("web3") // bridge between JS and Solidity
+const { Web3 } = require("web3")
+
+
+const web3 = new Web3(ganache.provider) //instance of Web3, tells to attempt to connect to a network;  add provider to connect
